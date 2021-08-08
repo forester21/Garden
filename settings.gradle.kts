e1e7a88d08d4d2d89d8d5c77ba4b229e146adc2e
FORESTER <forester2k12@bk.ru>
2021-08-08 21:13:43 +0300

    Вынес версии gradle плагинов в settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    plugins {
+        // Spring
+        id("org.springframework.boot") version "2.5.3"
+        id("io.spring.dependency-management") version "1.0.11.RELEASE"
+        kotlin("plugin.spring") version "1.5.21"
+
+        // Kotlin
+        kotlin("jvm") version "1.5.21"
+    }
+}
+
 rootProject.name = "garden"
 include("garden-webapp")
